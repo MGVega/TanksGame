c8ac0b06aa56618dab642f59f2d79119cca56fdd
agonzalezo03 <agonzalezobreo@gmail.com>
2023-10-27 17:45:22 +0200

    contador.js
    Creación del archivo contador.js con la funcion de addContador

---
+++ contador.js
@@ -0,0 +1,19 @@
+import {contador} from './script.js';
+
+
+export function addContador(){
+ 
+        let vidas1 = document.createElement("h2");
+        vidas1.id("j1")
+        vidas1.innerText("3")
+        contador.appendChild(vidas);
+
+        let vidas2 = document.createElement("h2");
+        vidas2.id("j2")
+        vidas2.innerText("3")
+        contador.appendChild(vidas);
+   
+    
+}
+
+export function
