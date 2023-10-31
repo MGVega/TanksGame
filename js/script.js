1a5ea1bb284788187c6e222a01e6b82102112e5a
Mario <145485899+MGVega@users.noreply.github.com>
2023-10-31 16:56:40 +0100

    Changed some links and using modules

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Imports
-import { drawImage } from "./modules/drawImage.js";
-import { clearRect } from "./modules/clearRect.js";
-import { WIDTH, HEIGHT, FACTOR } from "./modules/constants.js";
+import { drawImage } from "../modules/drawImage.js";
+import { clearRect } from "../modules/clearRect.js";
+import { WIDTH, HEIGHT, FACTOR } from "../modules/constants.js";
 
 const body = document.body;
 const tablero = document.querySelector('#tablero');
@@ -14,9 +14,23 @@ tablero.height = 800;
 //Indicamos el contexto y tipo de tablero 2Dimensiones
 let context = tablero.getContext("2d");
 
+const playerPrueba = {
+  url: "../img/player.png",
+  x: (WIDTH / 2) - 50,
+  y: HEIGHT - 150,
+  speed: 15
+};
+
+//Actualiza la página y se llama así mismo constantemente
+function update() {
+  console.log("update");
+  requestAnimationFrame(render);
+  requestAnimationFrame(update);
+}
+
 //Renderizar el juego: Limpiamos el contexto (tablero), dibujamos a los jugadores(drawImage)
 function render() {
-    clearRect(context); //Limpia
+    clearRect(context); //Limpia y pinta fondo negro
     //drawImage(context, player); //Jugador 1
     //drawImage(context, player); //Jugador 2
 
