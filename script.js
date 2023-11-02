8ecb8fe552516058eee13367c12ea542fb7454a6
agonzalezo03 <agonzalezobreo@gmail.com>
2023-11-02 17:31:10 +0100

    add player 2

--- script.js
+++ script.js
@@ -11,11 +11,18 @@ let context = myCanvas.getContext("2d");
 
 // Ejemplo movimiento jugador
 
-const player = {
+const player1 = {
   url: "./images/1arriba.png",
   x: (WIDTH / 2) - 50,
   y: HEIGHT - 150,
-  speed: 15
+  speed: 100
+};
+
+const player2 = {
+  url: "./images/1arriba.png",
+  x: (WIDTH / 2) - 100,
+  y: HEIGHT - 150,
+  speed: 100
 };
 
 
@@ -27,7 +34,8 @@ function update() {
 
 function render() {
   clearRect(context);
-  drawImage(context, player);
+  drawImage(context, player1);
+  drawImage(context, player2);
 }
 
 addEventListener("keydown", (event) => {
@@ -35,20 +43,41 @@ addEventListener("keydown", (event) => {
   let isRight = event.key === "ArrowRight";
   let isUp = event.key === "ArrowUp";
   let isDown = event.key === "ArrowDown";
+  
+  let isLeft2 = event.key === "a";
+  let isRight2 = event.key === "d";
+  let isUp2 = event.key === "w";
+  let isDown2 = event.key === "s";
 
   if (isLeft) {
-    player.x -= player.speed * FACTOR;
+    player1.x -= player1.speed * FACTOR;
   }
   if (isRight) {
-    player.x += player.speed * FACTOR;
+    player1.x += player1.speed * FACTOR;
   }
   if (isUp) {
-    player.y -= player.speed * FACTOR;
+    player1.y -= player1.speed * FACTOR;
   }
   if (isDown) {
-    player.y += player.speed * FACTOR;
+    player1.y += player1.speed * FACTOR;
+  }
+  
+  if (isLeft2) {
+    player2.x -= player2.speed * FACTOR;
+  }
+  if (isRight2) {
+    player2.x += player2.speed * FACTOR;
+  }
+  if (isUp2) {
+    player2.y -= player2.speed * FACTOR;
+  }
+  if (isDown2) {
+    player2.y += player2.speed * FACTOR;
   }
 });
+
+
+
 
 
 let wallpaper = new Image();
