1a4947e0dd9315ade26e8d53cde9cb33c97e085e
abdoonhdz <abdonhernandezgit@gmail.com>
2023-11-03 16:45:56 +0100

    create constants and limits on the canvas

--- modules/constants.js
+++ modules/constants.js
@@ -1,3 +1,7 @@
 export const WIDTH = 640;
 export const HEIGHT = 480;
 export const FACTOR = 0.20;
+export const X_IZQUIERDA = 0;
+export const X_DERECHA = WIDTH;
+export const Y_ARRIBA = 0;
+export const Y_ABAJO = HEIGHT;
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { drawImage } from "./modules/drawImage.js";
 import { clearRect } from "./modules/clearRect.js";
-import { WIDTH, HEIGHT, FACTOR } from "./modules/constants.js";
+import { WIDTH, HEIGHT, FACTOR, X_IZQUIERDA, X_DERECHA, Y_ARRIBA, Y_ABAJO } from "./modules/constants.js";
 import {  countdown } from "./old/js/contador.js";
 
 let secondsElement = document.getElementById("secondsElement");
@@ -47,41 +47,41 @@ addEventListener("keydown", (event) => {
   let isUp = event.key === "ArrowUp";
   let isDown = event.key === "ArrowDown";
   
-  let isLeft2 = event.key === "a";
-  let isRight2 = event.key === "d";
-  let isUp2 = event.key === "w";
-  let isDown2 = event.key === "s";
+  let isLeft2 = event.key.toLowerCase() === "a";
+  let isRight2 = event.key.toLowerCase() === "d";
+  let isUp2 = event.key.toLowerCase() === "w";
+  let isDown2 = event.key.toLowerCase() === "s";
 
-  if (isLeft) {
+  if (isLeft && player1.x > X_IZQUIERDA) {
     player1.x -= player1.speed * FACTOR;
     player1.url = "./images/1izquierda.png";
   }
-  if (isRight) {
+  if (isRight &&  player1.x < X_DERECHA - 100) {
     player1.x += player1.speed * FACTOR;
     player1.url = "./images/1derecha.png";
   }
-  if (isUp) {
+  if (isUp && player1.y > Y_ARRIBA) {
     player1.y -= player1.speed * FACTOR;
     player1.url = "./images/1arriba.png";
   }
-  if (isDown) {
+  if (isDown && player1.y < Y_ABAJO - 100) {
     player1.y += player1.speed * FACTOR;
     player1.url = "./images/1abajo.png";
   }
   
-  if (isLeft2) {
+  if (isLeft2 && player2.x > X_IZQUIERDA) {
     player2.x -= player2.speed * FACTOR;
     player2.url = "./images/2izquierda.png";
   }
-  if (isRight2) {
+  if (isRight2 && player2.x < X_DERECHA - 100) {
     player2.x += player2.speed * FACTOR;
     player2.url = "./images/2derecha.png";
   }
-  if (isUp2) {
+  if (isUp2 && player2.y > Y_ARRIBA) {
     player2.y -= player2.speed * FACTOR;
     player2.url = "./images/2arriba.png";
   }
-  if (isDown2) {
+  if (isDown2 && player2.y < Y_ABAJO - 100) {
     player2.y += player2.speed * FACTOR;
     player2.url = "./images/2abajo.png";
   }
